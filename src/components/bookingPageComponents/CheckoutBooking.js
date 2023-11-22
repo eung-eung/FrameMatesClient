ee1edc687ab3c75d6ce70b752579269963394976
eung-eung <boyvip5231@gmail.com>
2023-11-22 15:25:34 +0700

    order booking

--- src/components/bookingPageComponents/CheckoutBooking.js
+++ src/components/bookingPageComponents/CheckoutBooking.js
@@ -11,8 +11,8 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import QueryBuilderIcon from '@mui/icons-material/QueryBuilder';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import EventBusyIcon from '@mui/icons-material/EventBusy';
-export default function CheckoutBooking() {
-    const [step, setStep] = useState(2)
+import PaymentMethod from './PaymentMethod';
+export default function CheckoutBooking({ step, setStep, cancel }) {
     const [slot, setSlot] = useState('');
     const [time, setTime] = useState('')
     const handleChange = (event) => {
@@ -24,101 +24,108 @@ export default function CheckoutBooking() {
 
     return (
         <div>
-            <h2>Check out</h2>
-            <div className='step-by-step'>
-                {/* step 1 */}
-                <div className='step-item'>
-                    {step === 1 ? <CheckCircleIcon /> : <span style={{ color: "#ddd", border: "1px solid #ddd" }} className='holder-step'>1</span>}
-                    {step !== 1 ? <span style={{ color: "#ddd" }}>Information</span> : <span> Information</span>}
+            {step !== 3 && !cancel && <h2>Check out</h2>}
+            {!cancel &&
+                <div className='step-by-step'>
+                    {/* step 1 */}
+                    <div className='step-item'>
+                        {step >= 1 ? <CheckCircleIcon className='holder-step' /> : <span style={{ color: "#ddd", border: "1px solid #ddd" }} className='holder-step'>1</span>}
+                        {step !== 1 ? <span style={{ color: "#ddd" }}>Information</span> : <span> Information</span>}
+                    </div>
+
+                    {/* step 2 */}
+                    <div className='step-item'>
+                        {step >= 2 ? <CheckCircleIcon className='holder-step' /> : <span style={{ color: "#ddd", border: "1px solid #ddd" }} className='holder-step'>2</span>}
+                        {step !== 2 ? <span style={{ color: "#ddd" }}>Payment method</span> : <span>      Payment method</span>}
+
+                    </div>
+
+                    {/* step 3 */}
+                    <div className='step-item'>
+                        {step >= 3 ? <CheckCircleIcon className='holder-step' /> : <span style={{ color: "#ddd", border: "1px solid #ddd" }} className='holder-step'>3</span>}
+                        {step !== 3 ? <span style={{ color: "#ddd" }}>Complete</span> : <span>Complete</span>}
+                    </div>
+
                 </div>
 
-                {/* step 2 */}
-                <div className='step-item'>
-                    {step === 2 ? <CheckCircleIcon /> : <span style={{ color: "#ddd", border: "1px solid #ddd" }} className='holder-step'>2</span>}
-                    {step !== 2 ? <span style={{ color: "#ddd" }}>Payment method</span> : <span>      Payment method</span>}
+            }
 
-                </div>
+            {step === 1 && !cancel && <>
+                <div className='booking-studio'>
+                    <div className='booking-studio-header'>
+                        <p>artitum studio</p>
+                        <p className='price'>400$</p>
+                    </div>
+                    <div className='body'>
+                        <p className='sub-title'>Get professional photography in vintage style</p>
+                        <div className='studio-description'>
+                            <div className='studio-description-image'>
+                                <img src='/images/Frame74.png' />
+                            </div>
+                            <div className='studio-description-item'>
+                                <p className='studio-description-item-name'>Artium Studio</p>
+                                <p className='studio-description-item-slogan'>Vintage</p>
+                            </div>
+                            <div className='studio-description-category'>
+                                <div><CameraAltOutlinedIcon /></div>
+                                <div><BrushOutlinedIcon /></div>
 
-                {/* step 3 */}
-                <div className='step-item'>
-                    {step === 3 ? <CheckCircleIcon /> : <span style={{ color: "#ddd", border: "1px solid #ddd" }} className='holder-step'>3</span>}
-                    {step !== 3 ? <span style={{ color: "#ddd" }}>Complete</span> : <span>Complete</span>}
-                </div>
-
-            </div>
-
-            <div className='booking-studio'>
-                <div className='booking-studio-header'>
-                    <p>artitum studio</p>
-                    <p className='price'>400$</p>
-                </div>
-                <div className='body'>
-                    <p className='sub-title'>Get professional photography in vintage style</p>
-                    <div className='studio-description'>
-                        <div className='studio-description-image'>
-                            <img src='/images/Frame74.png' />
-                        </div>
-                        <div className='studio-description-item'>
-                            <p className='studio-description-item-name'>Artium Studio</p>
-                            <p className='studio-description-item-slogan'>Vintage</p>
-                        </div>
-                        <div className='studio-description-category'>
-                            <div><CameraAltOutlinedIcon /></div>
-                            <div><BrushOutlinedIcon /></div>
-
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className='datepicker-studio'>
-                    <div className='date-picker-box'>
-                        <LocalizationProvider dateAdapter={AdapterDayjs}>
-                            <DatePicker label="Pick a date" />
-                        </LocalizationProvider>
+                    <div className='datepicker-studio'>
+                        <div className='date-picker-box'>
+                            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                <DatePicker label="Pick a date" />
+                            </LocalizationProvider>
+                        </div>
+                        <div className='time-selects'>
+                            <FormControl fullWidth>
+                                <InputLabel id="demo-simple-select-label">Slot</InputLabel>
+                                <Select
+                                    labelId="demo-simple-select-label"
+                                    id="demo-simple-select"
+                                    value={slot}
+                                    label="Slot"
+                                    onChange={handleChange}
+
+                                >
+                                    <MenuItem value={10}>
+                                        <div className='option-times'>
+                                            <div className='option-times'>
+                                                <QueryBuilderIcon />
+                                                Start: 10:00
+                                            </div>
+                                            <div className='option-times'>
+                                                <EventBusyIcon />
+                                                End: 12:00
+                                            </div>
+
+                                        </div>
+                                    </MenuItem>
+                                    <MenuItem value={20}>
+                                        <div className='option-times'>
+                                            <div className='option-times'>
+                                                <QueryBuilderIcon />
+                                                Start: 11:00
+                                            </div>
+                                            <div className='option-times'>
+                                                <EventBusyIcon />
+                                                End: 12:00
+                                            </div>
+
+                                        </div>
+                                    </MenuItem>
+                                </Select>
+                            </FormControl>
+                        </div>
                     </div>
-                    <div className='time-selects'>
-                        <FormControl fullWidth>
-                            <InputLabel id="demo-simple-select-label">Slot</InputLabel>
-                            <Select
-                                labelId="demo-simple-select-label"
-                                id="demo-simple-select"
-                                value={slot}
-                                label="Slot"
-                                onChange={handleChange}
 
-                            >
-                                <MenuItem value={10}>
-                                    <div className='option-times'>
-                                        <div className='option-times'>
-                                            <QueryBuilderIcon />
-                                            Start: 10:00
-                                        </div>
-                                        <div className='option-times'>
-                                            <EventBusyIcon />
-                                            End: 12:00
-                                        </div>
 
-                                    </div>
-                                </MenuItem>
-                                <MenuItem value={20}>
-                                    <div className='option-times'>
-                                        <div className='option-times'>
-                                            <QueryBuilderIcon />
-                                            Start: 11:00
-                                        </div>
-                                        <div className='option-times'>
-                                            <EventBusyIcon />
-                                            End: 12:00
-                                        </div>
-
-                                    </div>
-                                </MenuItem>
-                            </Select>
-                        </FormControl>
-                    </div>
                 </div>
+            </>}
+            {step === 2 && !cancel && <PaymentMethod />}
 
-
-            </div>
         </div>
     )
 }
--- src/components/bookingPageComponents/OrderSummary.js
+++ src/components/bookingPageComponents/OrderSummary.js
@@ -1,7 +1,74 @@
 import React from 'react'
 
-export default function OrderSummary() {
+export default function OrderSummary({ handleBooking, step, handleCancel, cancel }) {
     return (
-        <div>OrderSummary</div>
+        <>
+            {(step != 3 && !cancel) &&
+                <div style={{ width: "30%" }}>
+                    <div className='order-summary-container'>
+                        {step === 1 && <>
+                            <h3 className='title'>Order summary</h3>
+                            <div className='order-middle'>
+                                <img src='/images/Frame74.png' />
+                                <p className='order-studio'>Artium</p>
+                                <p className='order-slogan'>Vintage</p>
+                                <div className='studio-review'>
+                                    <span className='review-total'>4.2</span>
+                                    <span className='review-p'>Very good 54 reviews</span>
+                                </div>
+                                <p className='review-p'>Your booking is protected by Framemates</p>
+                            </div>
+                            <div className='order-details'>
+                                <h2 style={{ fontSize: "16px", fontWeight: 500 }}>Price Details</h2>
+                                <div className='order-details-item'>
+                                    <span className='od-dt-item-name'>Booking price</span>
+                                    <span className='od-dt-item-value'>$400</span>
+                                </div>
+
+                                <div className='order-details-item'>
+                                    <span className='od-dt-item-name'>Discount</span>
+                                    <span className='od-dt-item-value'>$0</span>
+                                </div>
+
+                                <div className='order-details-item'>
+                                    <span className='od-dt-item-name'>Taxes</span>
+                                    <span className='od-dt-item-value'>$0</span>
+                                </div>
+
+
+                                <div className='order-details-item'>
+                                    <span className='od-dt-item-name'>Services Fee</span>
+                                    <span className='od-dt-item-value'>$0</span>
+                                </div>
+                            </div>
+                            <div className='order-total'>
+                                <span className='order-total-name'>Total</span>
+                                <span className='order-total-value'>$400</span>
+                            </div>
+                        </>}
+                        {step === 2 && <img className='qr-bank' src='/images/qr.jpg' />}
+                    </div>
+                    <div className='booking-btns'>
+                        <button className='book-btn' onClick={handleBooking}>Book</button>
+                        <button className='cancel-btn' onClick={handleCancel}>Cancel</button>
+                    </div>
+
+                </div>
+            }
+            {step === 3 && <div className='notify-box'>
+                <img src='https://i.gifer.com/7efs.gif' />
+                <div className='notify'>Payment Sucessfull</div>
+            </div>
+
+            }
+            {cancel && <div className='notify-box'>
+                <img src='https://assets.materialup.com/uploads/4491890e-ab53-4c68-b209-a08e683f9590/preview.gif' />
+                <div className='notify'>Sorry, Payment failed</div>
+                <span>Unfortunately, your order cannot be completed</span>
+            </div>
+
+            }
+        </>
+
     )
 }
